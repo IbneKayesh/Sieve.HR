0956be6a695bc4b8377a8b099d873a02fa86d46b
Md. Ibne Kayesh <mis95@mis.prangroup.com>
2023-01-09 09:48:39 +0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sieve.HR.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
