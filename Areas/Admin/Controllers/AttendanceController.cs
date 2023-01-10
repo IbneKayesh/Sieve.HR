58626cb578fc9110e0b7365f166ed14c2f27bdd7
Asad Quadir <skasadquadir@gmail.com>
2023-01-10 11:54:26 +0600

    HR_ATTENDANCE_SHEET - Only View

--- Areas/Admin/Controllers/AttendanceController.cs
+++ Areas/Admin/Controllers/AttendanceController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sieve.HR.Areas.Admin.Models;
+using Sieve.HR.Services.Db;
 
 namespace Sieve.HR.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class AttendanceController : Controller
     {
+        private readonly HRDbContext _context;
+        public AttendanceController(HRDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
-        }
+            IEnumerable<HR_ATTENDANCE_SHEET> objList = _context.HR_ATTENDANCE_SHEET;
+            return View(objList);
+        }        
     }
 }
