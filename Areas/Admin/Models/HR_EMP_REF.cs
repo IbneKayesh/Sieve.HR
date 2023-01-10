4ed4062fed46cfafa90d2fb38e57a5f7c2d7b4c3
Md. Ibne Kayesh <mis95@mis.prangroup.com>
2023-01-10 11:39:36 +0600

    test 1

--- Areas/Admin/Models/HR_EMP_REF.cs
+++ Areas/Admin/Models/HR_EMP_REF.cs
@@ -1,12 +1,51 @@
-﻿namespace Sieve.HR.Areas.Admin.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sieve.HR.Areas.Admin.Models
 {
     public class HR_EMP_REF
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "ID")]
         public int ID { get; set; }
+
+        [Display(Name = "Employee")]
+        [Required(ErrorMessage = "{0} is required")]
         public int EMP_ID { get; set; }
+
+
+        [Display(Name = "Employee Name")]
+        [NotMapped]
+        public string? FULL_NAME { get; set; }
+
+
+        [Display(Name = "Name")]
+        [Required(ErrorMessage = "{0} is required")]
+        [StringLength(50, ErrorMessage = "{0} length is between {2} and {1}", MinimumLength = 3)]
         public string? REF_NAME { get; set; }
+
+        [Display(Name = "Designation")]
+        [Required(ErrorMessage = "{0} is required")]
+        [StringLength(50, ErrorMessage = "{0} length is between {2} and {1}", MinimumLength = 3)]
         public string? DESIGNATION { get; set; }
+
+        [Display(Name = "Contact Number")]
+        [Required(ErrorMessage = "{0} is required")]
+        [StringLength(30, ErrorMessage = "{0} length is between {2} and {1}", MinimumLength = 10)]
         public string? REF_CONTACT { get; set; }
+
+        [Display(Name = "Email Address")]
+        [StringLength(50, ErrorMessage = "{0} length is between {2} and {1}", MinimumLength = 0)]
+        [DataType(DataType.EmailAddress)]
         public string? REF_EMAIL { get; set; }
+
+        [Display(Name = "Address")]
+        [StringLength(150, ErrorMessage = "{0} length is between {2} and {1}", MinimumLength = 0)]
+        public string? REF_ADDR { get; set; }
+
+        //Foreign Key
+        [ForeignKey("EMP_ID")]
+        public virtual HR_EMP_DETAIL EMP { get; set; } = null!;
     }
 }
---
+++ Migrations/20230110050301_init2.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sieve.HR.Migrations
+{
+    public partial class init2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "HR_EMP_REF",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EMP_ID = table.Column<int>(type: "int", nullable: false),
+                    REF_NAME = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    DESIGNATION = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    REF_CONTACT = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    REF_EMAIL = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    REF_ADDR = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HR_EMP_REF", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_HR_EMP_REF_HR_EMP_DETAIL_EMP_ID",
+                        column: x => x.EMP_ID,
+                        principalTable: "HR_EMP_DETAIL",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HR_EMP_REF_EMP_ID",
+                table: "HR_EMP_REF",
+                column: "EMP_ID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HR_EMP_REF");
+        }
+    }
+}
