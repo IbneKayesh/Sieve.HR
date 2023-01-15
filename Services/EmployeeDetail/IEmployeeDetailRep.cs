0b2a1b49a752b7930dee2be881d82b151dd6a9bf
Asad Quadir <skasadquadir@gmail.com>
2023-01-15 16:35:00 +0600

    Working On Employee Detail

---
+++ Services/EmployeeDetail/IEmployeeDetailRep.cs
@@ -0,0 +1,9 @@
+﻿using Sieve.HR.Areas.Admin.Models;
+using Sieve.HR.Infrastructure;
+
+namespace Sieve.HR.Services.EmployeeDetail
+{
+    public interface IEmployeeDetailRep : IGenericRepository<HR_EMP_DETAIL>
+    {
+    }
+}
